83ec243a02e047cc1c88137569679b2d38b5ddcd
arturokumura <arturokumura2008@gmail.com>
2026-02-18 21:57:48 -0300

    Order, Instant, ISO 8601 pt.2

--- src/main/java/com/artur/projetovendas/config/TestConfig.java
+++ src/main/java/com/artur/projetovendas/config/TestConfig.java
@@ -1,12 +1,16 @@
 package com.artur.projetovendas.config;
 
+import com.artur.projetovendas.entities.Order;
 import com.artur.projetovendas.entities.User;
+import com.artur.projetovendas.repositories.OrderRepository;
 import com.artur.projetovendas.repositories.UserRepository;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,6 +20,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @JsonIgnore
+    @Autowired
+    private OrderRepository orderRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -23,6 +30,11 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
         userRepository.saveAll(Arrays.asList(u1,u2));
+        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
     }
 }
---
+++ src/main/java/com/artur/projetovendas/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.artur.projetovendas.repositories;
+
+import com.artur.projetovendas.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
