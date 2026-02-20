6c6f4149116ab8cc3832d1b252504c6f6a61e56c
arturokumura <arturokumura2008@gmail.com>
2026-02-20 16:53:21 -0300

    Exception handling- delete

--- src/main/java/com/artur/projetovendas/services/UserService.java
+++ src/main/java/com/artur/projetovendas/services/UserService.java
@@ -2,9 +2,12 @@ package com.artur.projetovendas.services;
 
 import com.artur.projetovendas.entities.User;
 import com.artur.projetovendas.repositories.UserRepository;
+import com.artur.projetovendas.services.exceptions.DatabaseException;
 import com.artur.projetovendas.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-       repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
